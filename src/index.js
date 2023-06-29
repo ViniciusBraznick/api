7e48c7dbbf247f74eeb628c79ab79bd199186664
Vinicius Braznick <viniciusbraznick@gmail.com>
2023-06-28 21:45:15 -0300

    create errorHandler middleware

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ require('express-async-errors');
 const routes = require('./routes');
 
 const cors = require('./app/middlewares/cors');
+const erroHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  response.sendStatus(500);
-});
+app.use(erroHandler);
 
 app.listen(3001, () => console.log('Server started at http://localhost:3001'));
