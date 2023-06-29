3c565775d70dfe9782d25a401c69b0b3eb61d67a
Vinicius Braznick <viniciusbraznick@gmail.com>
2023-06-28 21:42:52 -0300

    create cors middleware

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ require('express-async-errors');
 
 const routes = require('./routes');
 
+const cors = require('./app/middlewares/cors');
+
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 app.use((error, request, response, next) => {
   response.sendStatus(500);
